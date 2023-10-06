c063d34b75ab1d04cd4b773a7eb803a691989813
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-06 11:34:57 -0700

    Update

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-    belongs_to :post
-    belongs_to :comment
+  belongs_to :post
+  belongs_to :comment
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    has_many :comments
-    has_many :posts, foreign_key: 'author_id'
+  has_many :comments
+  has_many :posts, foreign_key: 'author_id'
 end
