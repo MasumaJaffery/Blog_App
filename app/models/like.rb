e272cab608fd059fa58d7c3fb3e62bc26401a3dd
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-06 13:35:27 -0700

    Update

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,11 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :comment
+  after_save :Update_like_count
+
+  private
+
+  def Update_like_count
+    Post.increment_counter(:likes_counter, post_id)
+  end
 end
