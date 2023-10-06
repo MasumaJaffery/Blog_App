bbbc2ae36d57e7abfb065fba1140345979bf6e4c
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-06 13:42:26 -0700

    fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  after_save :Update_comment_count
+  after_save :update_comment_count
 
   private
 
-  def Update_comment_count
+  def update_comment_count
     Post.increment_counter(:comments_counter, post_id)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,11 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :comment
-  after_save :Update_like_count
+  after_save :update_like_count
 
   private
 
-  def Update_like_count
+  def update_like_count
     Post.increment_counter(:likes_counter, post_id)
   end
 end
