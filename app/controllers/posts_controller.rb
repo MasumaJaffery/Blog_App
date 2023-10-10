51c1d182d8e0723ec8daec652151591cb58823ee
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-10 08:51:43 -0700

    refactor code for current req + add route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,35 +9,4 @@ class PostsController < ApplicationController
     @users = User.find(params[:id])
     @posts = @users.posts.find(params[:id])
   end
-  # Here we define a functionality for new user!
-  def new
-    @users = new_user
-    @posts = @users.posts.build
-  end
-  # Here we define a functionality to create a post!
-  def create
-    @users = new_user
-    @posts = Post.new(
-      author: @users,
-      title: params[:post][:title],
-      text: params[:post][:text],
-      likes_counter: 0,
-      :comments_counter: 0
-    )
-    if @posts.save
-       flash.now[:success] = 'You have created a new post!'
-       # it will redirect to a page of users!
-       redirect_to users_posts_path(@users)
-    else
-        # Handles Error
-        flash.now[:error] = 'Encounter error while create a new post!'
-        # render a new (updated) template!
-        render new
-    end
-  end
-  private
-  # Mechanism to post data
-  def post_data
-    params.require(:post).permit(:title, :text)
-  end
 end
