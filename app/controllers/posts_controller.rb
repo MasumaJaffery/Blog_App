79baec92434134d724f7639057f4066bc3594737
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-10 10:25:46 -0700

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,6 @@
 class PostsController < ApplicationController
-    # This is a way to authenticate a user and %i[new create] is about to control a controller action!
-    before_action :authenticate_user!, only: %i[new create]
   def index
-    @users = User.find(params[:id])
-    @posts = @users.posts
   end
   def show
-    @users = User.find(params[:id])
-    @posts = @users.posts.find(params[:id])
-  end
+end
 end
