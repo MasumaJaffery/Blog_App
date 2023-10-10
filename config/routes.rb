d868587afa0bdbb41aca1d931dbf7cae9e3cc40d
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-10 12:50:10 -0700

    bundle install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
+  # %i is used to create an array in rails,
+  # do is used here to define flow of code execution ans used to block of cose it is not a loop!
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show]
   end
