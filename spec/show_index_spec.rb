daa738471a41a82e1a07766319fc60b9b389ba78
Javivy <javierralejandroortga@gmail.com>
2023-10-17 18:12:19 -0400

    Add tests for 'I can see the first comments on a post.'

--- spec/show_index_spec.rb
+++ spec/show_index_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Posts', type: :feature do
   before do
     @user = FactoryBot.create(:user, name: 'Masuma', photo_url: 'https://robohash.org/taryn.wehner.png?size=120x120&set=set1', posts_counter: 0)
     @post = FactoryBot.create(:post, title: 'Sample Post', text: 'Lorem ipsum', author: @user)
+    @comment = FactoryBot.create(:comment, text: 'Sample comment', user: @user, post: @post)
   end
 
   describe 'index page' do
@@ -25,6 +26,16 @@ RSpec.describe 'Posts', type: :feature do
     it 'can see a post\'s title.' do
       visit user_posts_path(user_id: @user.id)
       expect(page).to have_content(@post.title)
+    end
+
+    it 'can see a post\'s body.' do
+      visit user_posts_path(user_id: @user.id)
+      expect(page).to have_content(@post.text)
+    end
+
+    it 'can see the first comment of a post' do
+      visit user_posts_path(user_id: @user.id)
+      expect(page).to have_content(@comment.text)
     end
   end
 end
