e45dd6518955441bcd373c529064cf65af4d45a3
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-20 03:00:26 +0500

    Add Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'registrations'
-  }
+    registrations: 'users/registrations'
+}
 root 'users#index'
 
 resources :users, only: %i[index show] do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 620231007121613) do
+ActiveRecord::Schema[7.0].define(version: 620231007121614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,10 @@ ActiveRecord::Schema[7.0].define(version: 620231007121613) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email", "reset_password_token"], name: "index_users_on_email_and_reset_password_token", unique: true
   end
 
