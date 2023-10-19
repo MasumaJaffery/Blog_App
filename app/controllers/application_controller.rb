85311588d7da329ab10a2f2c14856a66139afad6
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-20 03:13:43 +0500

    Application Controller Update!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  secure_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_fliter.permit(:sign_up, keys: [:name, :email, :password])
+    devise_parameter_fliter.permit(:account_update, keys: [:name, :email, :password, :current_password])
+  end
 end
