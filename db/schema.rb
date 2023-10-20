f91ebcfbb4821c0284053b8bad1e22c513d37c7a
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-21 02:05:16 +0500

    Add Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 620231007121614) do
+ActiveRecord::Schema[7.0].define(version: 620231007121615) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 620231007121614) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "role"
     t.index ["email", "reset_password_token"], name: "index_users_on_email_and_reset_password_token", unique: true
   end
 
