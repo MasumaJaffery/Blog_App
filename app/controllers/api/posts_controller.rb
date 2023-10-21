52618a74c7a1df7ac362ec296aaa8f68be29333c
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-21 20:41:23 +0500

    Update and Fiz

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,26 +1,22 @@
 module Api
-    class PostsController < ApplicationController
-      before_action :set_user,
-      before_action :set_post
-  
-      def index
-        @posts = @user.posts
-        render json: @posts
-      end
-  
-      def show
-        render json: @post
-      end
-  
-      private
-  
-      def set_user
-        @user = User.find(params[:user_id])
-      end
-  
-      def set_post
-        @post = @user.posts.find(params[:id])
+  class PostsController < ApplicationController
+    before_action :set_user
+
+    def index
+      @posts = @user.posts
+      render json: @posts
+    end
+
+    private
+
+    def set_user
+      @user = current_user
+      User.find_by(id: params[:user_id])
+    
+      if @user.nil?
+        render json: { error: 'User not found' }, status: :not_found
       end
     end
+    
   end
-  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,22 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
-}
-root 'users#index'
+  }
 
-resources :users, only: %i[index show] do
-  resources :posts, only: %i[index show new create destroy] do
-    resources :comments, only: %i[index show new create edit update destroy]
-    resources :likes, only: %i[index show create destroy]
+  root 'users#index'
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+      resources :likes, only: [:index, :show, :create, :destroy]
+    end
   end
+
+  # Define the API routes after the standard routes
   namespace :api, defaults: { format: :json } do
     resources :sessions
     resources :posts, only: [:index] do
       resources :comments, only: [:index, :create]
     end
   end
-end  
 end
