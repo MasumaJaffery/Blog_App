1dfbc9d32150b1b93a2cc25337cc42f2c18c8bba
Javivy <javierralejandroortga@gmail.com>
2023-10-21 14:23:18 -0400

    List comments in JSON, create a comment for a given post in json

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,4 +1,34 @@
 module Api
-    class CommentsController < ApplicationController
+  class CommentsController < ApplicationController
+    def index
+      @coments = @post.comments
+      render json: @comments
+    end
+
+    def create
+      @comment = @post.comments.build(comment_params)
+      @comment.user = current_user
+  
+      if @comment.save
+        render json: @comment, status: :created
+      else
+        render json: @comment.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+  
+      def set_user
+        @user = User.find(params[:user_id])
+      end
+  
+      def set_post
+        @post = @user.posts.find(params[:post_id])
+      end
+
+      def comment_params
+        params.require(:comment).permit(:text)
+      end
   end
+end
   
